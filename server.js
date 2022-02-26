aea7441ceca48d29f7ab30277c1eb6c2a2a682db
anmolraidewan <anmoldewanrai@gmail.com>
2022-02-26 20:08:57 +0530

    notes taken

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+// core node modules
 const http = require("http");
 const fs = require("fs");
 
+// 3rd party packages
+const _ = require("lodash");
+
 const server = http.createServer((req, res) => {
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log("hello");
+  });
+
+  greet();
+  greet();
+
   // setting html header
   res.setHeader("Content-type", "text/html");
 
